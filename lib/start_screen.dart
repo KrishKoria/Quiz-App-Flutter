6d60374d873d00689bfcdd2707d750f76c13aeeb
KrishKoria <krishkoria2004@gmail.com>
2023-12-31 19:30:42 +0530

    Readied App for Conditional Rendering, Added Quiz.dart and Questions.dart files, Added Icon in buttons and transparency in image

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,20 +12,38 @@ class StartScreen extends StatelessWidget {
         children: [
           Image.asset(
             'assets/images/quiz-logo.png',
-            width: 200,
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
-          const SizedBox(height: 40),
+          const SizedBox(height: 80),
           const Text(
             "Learn Flutter in a fun way!",
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          TextButton(
-            child: const Text(
-              'Start Quiz',
-              style: TextStyle(fontSize: 16, color: Colors.white),
+            style: TextStyle(
+              fontSize: 24,
+              color: Color.fromARGB(255, 238, 215, 253),
             ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            icon: const Icon(Icons.play_arrow),
             onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              side: const BorderSide(
+                color: Color.fromARGB(255, 238, 215, 253),
+                width: 2,
+              ),
+              foregroundColor: const Color.fromARGB(255, 238, 215, 253),
+            ),
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
           ),
         ],
       ),
